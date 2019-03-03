1df58f1eb4027c58b6457e1e11babd4b2839f079
ghost0fharambe <43300063+ghost0fharambe@users.noreply.github.com>
2019-03-03 14:51:27 -0700

    added function to shuffle images and on click function

--- src/App.js
+++ src/App.js
@@ -5,20 +5,51 @@ import Title from "./components/Title";
 import images from "./pictures.json";
 import './App.css';
 
-class App extends Component {
+var guessedArray = [];
 
-  state = {
-    images
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      images,
+      guessedArray
+    }
   }
 
-  
+  shuffleImages = () => {
+    let array = [];
+    let images = this.state.images;
+
+    for (let i in images) {
+      let randomIndex = Math.floor(Math.random() * images.length);
+      while (array.includes(images[randomIndex])) {
+        randomIndex = Math.floor(Math.random() * images.length);
+      }
+      array[i] = images[randomIndex];
+      this.setState({ images: array });
+    };
+  };
+
+  makeGuess = id => {
+    this.shuffleImages();
+    let guessed = this.state.guessedArray;
+    console.log(this.state.guessedArray)
+    for (let i = 0; i < guessed.length; i++) {
+      if (guessed[i] === id) {
+        console.log("lost");
+      } else {
+        console.log("test");
+        this.shuffleImages();
+      }
+    }
+  }
 
   render() {
     return (
       <Wrapper>
         <Title>Archer Memory Game!</Title>
         {this.state.images.map(image => (
-          <Image src={image.src} key={image.id} id={image.id} />
+          <Image src={image.src} key={image.id} id={image.id} guess={this.makeGuess} />
         ))}
       </Wrapper>
     );
--- src/components/Image/index.js
+++ src/components/Image/index.js
@@ -4,7 +4,7 @@ import "./styles.css";
 function Image(props) {
 	return (
 		<div className="img-container">
-			<img alt={props.name} src={props.src} key={props.id} className="img-thumbnail"/>
+			<img alt={props.name} src={props.src} key={props.id} className="img-thumbnail" onClick={() => props.guess(props.id)}/>
 		</div>
 	);
 };
